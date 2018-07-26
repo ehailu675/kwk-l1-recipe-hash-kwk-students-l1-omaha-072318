200af280a3b8f0441b3372a18830f47bd7c95d14
ehailu675 <elisabethahailu@gmail.com>
2018-07-26 20:19:10 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -11,7 +11,7 @@ perfect_10_recipe = {
     :slivered_almonds_lightly_toasted => "1/4 cup",
     :mini_dark_chocolate_chips => "3/4 cup"
     :olive_oil => "1/2 cup"
-    :agave => "1/4 cup"
+    :agave => "1/4 cup" }
 #  - almond flour - 2 cups
 #  - gluten freewhole oats - 1 cup
 #  - kosher salt - 1/2 tea spoon
