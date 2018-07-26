a67ff28d6d2ee5a3102a1f145c77b7d585f2265d
ehailu675 <elisabethahailu@gmail.com>
2018-07-26 20:25:35 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -1,8 +1,8 @@
 #code your solutions here
 
 #1. Create a hash called `perfect_10_recipe` that has the following data:
-perfect_10_recipe = ({ 
-    :almond_flour => "2 cups",
+perfect_10_recipe = 
+  {:almond_flour => "2 cups",
     :gluten_free_whole_oats => "1 cup",
     :kosher_salt => "1/2 tea spoon",
     :baking_powder => "1/2 tea spoon",
@@ -12,7 +12,7 @@ perfect_10_recipe = ({
     :mini_dark_chocolate_chips => "3/4 cup", 
     :olive_oil => "1/2 cup",
     :agave => "1/4 cup" 
-  })
+  }
 #  - almond flour - 2 cups
 #  - gluten freewhole oats - 1 cup
 #  - kosher salt - 1/2 tea spoon
