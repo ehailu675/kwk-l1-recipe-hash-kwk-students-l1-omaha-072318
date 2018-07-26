9936c1bd03332ce563ac38903f637969423e409b
ehailu675 <elisabethahailu@gmail.com>
2018-07-26 20:19:37 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -9,8 +9,8 @@ perfect_10_recipe = {
     :baking_soda => "1/4 tea spoon",
     :Xanthan_Gum => "1/2 tea spoon",
     :slivered_almonds_lightly_toasted => "1/4 cup",
-    :mini_dark_chocolate_chips => "3/4 cup"
-    :olive_oil => "1/2 cup"
+    :mini_dark_chocolate_chips => "3/4 cup", 
+    :olive_oil => "1/2 cup",
     :agave => "1/4 cup" }
 #  - almond flour - 2 cups
 #  - gluten freewhole oats - 1 cup
